dba70570b7c3740b9a2f2ed3b28f54c88002a0ce
Humberto <humberto.peluso@yahoo.com.br>
2021-01-21 15:44:44 -0300

    Board exception; Programação defensiva

--- src/application/boardgame/Board.java
+++ src/application/boardgame/Board.java
@@ -6,6 +6,8 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int rows, int columns) {
+        if(rows < 1 || columns <1)
+            throw new BoardException("Error creating boar: at least one row and one column is required");
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
@@ -15,19 +17,13 @@ public class Board {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
     public int getColumns() {
         return columns;
     }
 
-    public void setColumns(int columns) {
-        this.columns = columns;
-    }
-
     public Piece piece(int row, int column){
+        if(!positionExists(row, column))
+            throw new BoardException("Position not on the board.");
         return pieces[row][column];
     }
 
@@ -36,7 +32,24 @@ public class Board {
     }
 
     public void placePiece(Piece piece, Position position){
+        if(thereIsAPiece(position))
+            throw new BoardException("There is a piece on position: "+ position);
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
     }
+
+    private boolean positionExists(int row, int column){
+        return row >= 0 && row < rows && column >= 0 &&  column < columns;
+    }
+
+    public boolean positionExists(Position position){
+        return positionExists(position.getRow(), position.getColumn());
+    }
+
+    public boolean thereIsAPiece(Position position){
+        if(thereIsAPiece(position))
+            throw new BoardException("There is a piece on position: "+ position);
+       return piece(position) != null;
+    }
+
 }
---
+++ src/application/boardgame/BoardException.java
@@ -0,0 +1,8 @@
+package application.boardgame;
+
+public class BoardException extends RuntimeException{
+
+    public BoardException(String message) {
+        super(message);
+    }
+}
